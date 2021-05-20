46318e8aff813cdd09856b9064aba8003774046f
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2021-05-19 23:56:12 -0500

    Add ChunkedUpload class and test server file.

---
+++ index.js
@@ -0,0 +1,66 @@
+const Busboy = require('busboy');
+const fs = require('fs');
+const util = require('util');
+
+const { parse } = require('content-range');
+const mergeFiles = require('merge-files');
+
+class ChunkedUpload {
+  constructor(fileFields, chunkIdHeader, chunkSizeHeader) {
+    this.fileFields = fileFields;
+    this.chunkIdHeader = chunkIdHeader;
+    this.chunkSizeHeader = chunkSizeHeader;
+  }
+
+  _isLastPart = contentRange => {
+    return contentRange.size === contentRange.end;
+  }
+
+  _buildOriginalFile = (chunkId, chunkSize, contentRange, filename) => {
+    const totalParts = Math.floor(contentRange.size / chunkSize) + 1;
+
+    const parts = [...Array(totalParts).keys()]; // [0, 1, 2, ..., totalParts]
+    const partsFilenames = parts.map(part =>
+        util.format('%s-%i.part', chunkId, part)
+    );
+
+    return mergeFiles(partsFilenames, filename).then(_ => {
+      partsFilenames.forEach(filename => fs.unlinkSync(filename));
+    });
+  }
+
+  makeMiddleware = () => {
+    return (req, res, next) => {
+      const busboy = new Busboy({ headers: req.headers });
+      busboy.on('file', (fieldName, file, filename, encoding, mimetype) => {
+
+        if (!this.fileFields.includes(fieldName)) {  // current field is not handled
+          next();
+          return;
+        }
+
+        const chunkId = req.headers[this.chunkIdHeader];
+        const chunkSize = req.headers[this.chunkSizeHeader];
+        const contentRange = parse(req.headers['content-range']);
+
+        const part = contentRange.start / chunkSize;
+        const partFilename = util.format('%s-%i.part', chunkId, part);
+
+        const writableStream = fs.createWriteStream(partFilename);
+        file.pipe(writableStream);
+
+        if (this._isLastPart(contentRange)) {
+          this._buildOriginalFile(chunkId, chunkSize, contentRange, filename).then(_ => {
+            next();
+          });
+        } else {
+          next();
+        }
+      });
+
+      req.pipe(busboy);
+    };
+  }
+}
+
+module.exports = ChunkedUpload;
---
+++ test-server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const ChunkedUpload = require('./index');
+
+const app = express();
+
+const chunkedUpload = new ChunkedUpload(
+    'file', 'chunk-id', 'chunk-size'
+);
+
+app.post('/', chunkedUpload.makeMiddleware(), (req, res) => {
+    res.send('Hello world.');
+});
+
+console.log('Running server on port 3000');
+app.listen(3000);
